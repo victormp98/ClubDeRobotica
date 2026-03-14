e1447a06cf737a13797f8c1abdce06b454ef4fa0
Victor Manuel <victor.manuel.pp98@gmail.com>
2026-03-13 22:53:55 -0500

    Add About page with Mission, Vision, Values, Team sections

--- app/views/main.py
+++ app/views/main.py
@@ -5,3 +5,7 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/about')
+def about():
+    return render_template('about.html')
